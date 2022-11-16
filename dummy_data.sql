32d5f907f9c8296d5e0287fa3bc85c905c9d7c32
Nikhil Mohite <nikhilmohitelhs@gmail.com>
2022-11-16 22:25:56 +0530

    bugfix: update works now

--- dummy_data.sql
+++ dummy_data.sql
@@ -1,4 +1,3 @@
--- done
 insert into food_item Values('01','thaali','400');
 insert into food_item Values('02','pizza','250');
 insert into food_item Values('03','ghee rice','120');
@@ -7,13 +6,12 @@ insert into food_item Values('05','chicken biryani','180');
 insert into food_item Values('06','mutton thaali','320');
 insert into food_item Values('07','surma fish','240');
  
--- done
+
 insert into offers Values('01','winter vaction', '1001','1006','20','2022-12-20','2022-12-30');
 insert into offers Values('02','diwali offer', '1005','1001','15','2022-10-01','2022-12-31'); 
 insert into offers Values('03','special offer', '1007','1007','25','2022-11-01','2022-12-31');
 
 
--- done
 insert into resort Values ('1001',"The Dukes Retreat",'Lonavala',5,1999);
 insert into resort Values ('1002',"Ferreira Resort",'Lonavala',4,1799);
 insert into resort Values ('1003',"Villa San Lorentz",'Lonavala',5,1699);
@@ -26,7 +24,6 @@ insert into resort Values ('1009',"Alurkar Resort",'Belgaum',5,1499);
 insert into resort Values ('1010',"Gavkari",'Belgaum',5,999);
 
 
--- done
 insert into room_service Values('101','chotu','1001');
 insert into room_service Values('102','bhola','1002');
 insert into room_service Values('103','brijesh','1003');
@@ -39,7 +36,6 @@ insert into room_service Values('109','keshav','1006');
 insert into room_service Values('110','sharad','1009');
 insert into room_service Values('111','munna','1010');
 
--- done
 insert into customer Values('1001','narendra','','modi','gujarat' ,'modi@gmail.com' ,'1234567890','106');
 insert into customer Values('1002','amit','','shah','gujarat' ,'shah@gmail.com' ,'1234567890','102');
 insert into customer Values('1003','atal','b','vajpayee','bihar' ,'vajpayee@gmail.com' ,'1234567890','103');
@@ -48,7 +44,7 @@ insert into customer Values('1005','yogi','','adityanath','uttar pradesh' ,'yogi
 insert into customer Values('1006','balasaheb','','thakre','mumbai' ,'thakre@gmail.com' ,'1234567890','101');
 insert into customer Values('1007','basavraj','','bommai','karnataka' ,'bommai@gmail.com' ,'1234567890','102');
 
--- done
+
 insert into bill Values('250707244234','2022-11-21','1001','upi');
 insert into bill Values('202622637838','2022-11-22','1002','debit card');
 insert into bill Values('644161913172','2022-11-15','1003','credit card');
@@ -56,7 +52,7 @@ insert into bill Values('988602103725','2022-11-21','1004','upi');
 insert into bill Values('859741505883','2022-11-21','1005','cash');
 insert into bill Values('346488919858','2022-11-26','1006','credit card');
 
--- done
+
 insert into orders Values('1001','1','3','2022-11-21  10:55:05');
 insert into orders Values('1002','4','4','2022-11-21  13:56:55');
 insert into orders Values('1003','5','2','2022-11-10  12:35:05');
@@ -65,7 +61,6 @@ insert into orders Values('1002','3','1','2022-11-18  07:24:05');
 insert into orders Values('1006','4','4','2022-11-25  09:18:05');
 
 
--- done
 insert into relatives Values('1002','bhaskar bhat','m','friend');
 insert into relatives Values('1003','vijay verma','m','friend');
 insert into relatives Values('1003','sujay patil','m','friend');
@@ -77,7 +72,6 @@ insert into relatives Values('1005','shilpa trivedi','f','colleague');
 insert into relatives Values('1006','manthan patil','m','friend');
 
 
--- done
 insert into reservation values('1001','1001','2022-11-11','2022-11-21',15592);
 insert into reservation values('1002','1002','2022-11-15','2022-11-22',12593);
 insert into reservation values('1003','1003','2022-11-15','2022-11-227',11893);
--- pages/3_🧿_Update.py
+++ pages/3_🧿_Update.py
@@ -36,116 +36,85 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-table = st.selectbox("delete_table", ("Customer", "Resort"), label_visibility="hidden")
-if table == "Customer":
-    st.subheader("Enter the Customer ID")
-    cid = st.number_input(
-        "cid", min_value=1000, max_value=9999, label_visibility="hidden"
-    )
 
-    c_btn = st.button("FETCH")
+st.subheader("Select/Enter the Customer ID")
 
-    if c_btn:
-        try:
-            q = "select * from customer where cid=%d" % cid
-            db_cursor.execute(q)
-            r = db_cursor.fetchone()
-            if r is not None:
-                cid, fname, minit, lname, addr, mail, pno, waiter_id = r
+db_cursor.execute("select cid from customer")
 
-                st.write("---")
-                with st.form(key="update_form_cust"):
-                    st.subheader("Update Form")
+cid = st.selectbox(
+    "cid",
+    [int(i[0]) for i in db_cursor.fetchall()],
+    label_visibility="hidden",
+)
 
-                    col1, col2, col3 = st.columns(3)
-                    with col1:
-                        fname = st.text_input(
-                            "fname",
-                            fname,
-                            placeholder="Enter your First Name",
-                            label_visibility="hidden",
-                        )
-                    with col2:
-                        minit = st.text_input(
-                            "minit",
-                            minit,
-                            placeholder="Middle Name initials",
-                            label_visibility="hidden",
-                        )
-                    with col3:
-                        lname = st.text_input(
-                            "lname",
-                            lname,
-                            placeholder="Enter your Last Name",
-                            label_visibility="hidden",
-                        )
+st.write("---")
 
-                    address = st.text_input(
-                        "address",
-                        addr,
-                        placeholder="Enter your address",
-                        label_visibility="hidden",
-                    )
+try:
+    db_cursor.execute(f"select * from customer where cid={cid}")
+    cid, fname, minit, lname, addr, mail, pno, waiter_id = db_cursor.fetchone()
 
-                    col1, col2 = st.columns(2)
-                    with col1:
-                        email = st.text_input(
-                            "email",
-                            mail,
-                            placeholder="Enter your Email",
-                            label_visibility="hidden",
-                            type="default",
-                        )
-                    with col2:
-                        pno = st.text_input(
-                            "pno",
-                            pno,
-                            placeholder="Enter your Mobile number",
-                            label_visibility="hidden",
-                        )
+    with st.form(key="update_form_cust"):
+        st.subheader("Update Form")
 
-                    update_btn = st.form_submit_button("UPDATE")
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            fname = st.text_input(
+                "fname",
+                fname,
+                placeholder="Enter your First Name",
+                label_visibility="hidden",
+            )
+        with col2:
+            minit = st.text_input(
+                "minit",
+                minit,
+                placeholder="Middle Name initials",
+                label_visibility="hidden",
+            )
+        with col3:
+            lname = st.text_input(
+                "lname",
+                lname,
+                placeholder="Enter your Last Name",
+                label_visibility="hidden",
+            )
 
-                    if update_btn:
-                        st.write("Updating...")
-                        try:
-                            q = f"""update table set 
-                            fname={fname},
-                            minit={minit},
-                            lname={lname},
-                            address={addr},
-                            email={mail},
-                            contactNo={pno},
-                            waiter_id={waiter_id}
-                            where cid={cid}
-                            """
-                            db_cursor.execute(q)
-                            db.commit()
-                            st.success("Update successfull")
-                        except Exception as e:
-                            st.error(e)
-
-            else:
-                st.error(f"Entry not found for {cid}")
-        except Exception as e:
-            st.error(e)
-else:
-    with st.form(key="delete_form_resort"):
-        st.subheader("Enter the Resort ID")
-        resort_id = st.number_input(
-            "resort_id", min_value=1000, max_value=9999, label_visibility="hidden"
+        address = st.text_input(
+            "address",
+            addr,
+            placeholder="Enter your address",
+            label_visibility="hidden",
         )
 
-        c_btn = st.form_submit_button("DELETE")
+        col1, col2 = st.columns(2)
+        with col1:
+            email = st.text_input(
+                "email",
+                mail,
+                placeholder="Enter your Email",
+                label_visibility="hidden",
+                type="default",
+            )
+        with col2:
+            pno = st.text_input(
+                "pno",
+                pno,
+                placeholder="Enter your Mobile number",
+                label_visibility="hidden",
+            )
 
-        if c_btn:
-            try:
-                q = f"delete from resort where resort_id={resort_id}"
-                db_cursor.execute(q)
-                db.commit()
-                st.success(f"Resort {resort_id} deleted successfully")
-            except Exception as e:
-                st.error(e)
+        update_btn = st.form_submit_button("UPDATE")
+        if update_btn:
+            q = f"""
+            update customer set fname="{fname}", minit="{minit}", lname="{lname}", address="{address}", email="{mail}", contactNo="{pno}", waiter_id="{waiter_id}" where cid={cid}
+            """
+            db_cursor.execute(q)
+            db.commit()
+            st.success(f"Info updated successfully for Customer with {cid}")
+
+except Exception as e:
+    st.error(e)
+
 
 db.close()
 print("DB connection closed")
